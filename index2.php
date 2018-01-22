e752235cf11c0a2cbb17ffce4ee1ec2da1e4b7b3
Abishak Sharma <abishaksharma70@gmail.com>
2018-01-22 14:28:56 +1030

    Few code changes

--- index2.php
+++ index2.php
@@ -47,7 +47,7 @@ Include: Full name(Abishak Sharma),(Twitter: @ItsAbishak), Website(abishaksharma
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-<link href="http://www.kliptu.com/demo/goo_gl/assets/css/jquery-ui-1.10.3.css" rel="stylesheet">
+<link href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css" rel="stylesheet">
 
 <link href="style.css" rel="stylesheet"></head><body>
 <header>
@@ -60,14 +60,14 @@ Include: Full name(Abishak Sharma),(Twitter: @ItsAbishak), Website(abishaksharma
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">
+              <a class="navbar-brand" href="index2.php">
               	<span class="Bree_Serif" style="font-weight: bold; font-size:1.4em; letter-spacing:1.5px;">Abishak</span> <span class="title">URL SHORTENER</span>
               </a>
             </div>
            <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/login">LOGIN</a></li>
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/register">SIGN UP</a></li>
+                    <li><a href="login">LOGIN</a></li>
+                    <li><a href="register">SIGN UP</a></li>
                 </ul>
             </div>    -->                                        
         </nav>
@@ -119,11 +119,11 @@ Include: Full name(Abishak Sharma),(Twitter: @ItsAbishak), Website(abishaksharma
     	<div class="row">
         	<div class="col-md-12">
             	<ul class="nav_hor">
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/page/help">HELP</a></li>
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/page/reportspam">REPORT SPAM</a></li>
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/page/privacy">PRIVACY POLICY</a></li>
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/page/usagepolicy">ACCEPTABLE USAGE POLICY</a></li>
-                    <li><a href="http://www.kliptu.com/demo/goo_gl/page/terms">TERMS OF SERVICE</a></li>
+                    <li><a href="help">HELP</a></li>
+                    <li><a href="reportspam">REPORT SPAM</a></li>
+                    <li><a href="privacy">PRIVACY POLICY</a></li>
+                    <li><a href="usagepolicy">ACCEPTABLE USAGE POLICY</a></li>
+                    <li><a href="terms">TERMS OF SERVICE</a></li>
                 </ul>
             </div>
         </div>
@@ -190,9 +190,9 @@ Include: Full name(Abishak Sharma),(Twitter: @ItsAbishak), Website(abishaksharma
     </div>
   </div>
 </div><!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="http://www.kliptu.com/demo/goo_gl/assets/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="http://www.kliptu.com/demo/goo_gl/assets/js/jquery-ui-1.10.3.min.js"></script>
-<script src="http://www.kliptu.com/demo/goo_gl/assets/bootstrap-3.3.6/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="http://www.kliptu.com/demo/goo_gl/assets/js/jquery-qrcode-0.14.0.js"></script>
 
